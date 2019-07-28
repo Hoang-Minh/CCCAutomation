532dbbf71e77940134bab1fa8de17266d9688cb1
Minh Nguyen <MNguyen1@win.cccis.com>
2019-07-28 12:00:49 -0700

    refactor code, modulalize androifFooter and iosFooter into one footer

--- routes/android.js
+++ routes/android.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("partials/table", {type: "android"});
+    res.render("partials/table", 
+    {
+        type: "android",
+        file: "/js/androidTable.js"
+    });
 });
 
 module.exports = router;
--- routes/ios.js
+++ routes/ios.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("partials/table", {type: "ios"});
+    res.render("partials/table", 
+    {
+        type: "ios",
+        file: "/js/iosTable.js"
+    });
 });
 
 module.exports = router;
