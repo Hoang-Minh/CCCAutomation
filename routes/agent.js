8553cc392002ddb311d2a9b63eeda26e344b906b
Hoang-Minh <quataoxanh83@gmail.com>
2019-12-02 23:58:33 -0800

    clean up ejs file when displaying agents

--- routes/agent.js
+++ routes/agent.js
@@ -2,17 +2,16 @@ const express = require("express");
 const router = express.Router();
 const agent = require("../public/js/agent");
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {    
+    res.render("agent/agent");
+});
+
+router.get("/findAll", async(req, res) => {
     let agentPoolName = await agent.getAgentPoolName(process.env.AGENT_POOL_NAME);
-    let id = agentPoolName[0].id;
-    // console.log(id);
+    let id = agentPoolName[0].id;    
 
     let agents = await agent.getAgents(id);
-    
-    res.render("agent/agent", 
-    {        
-        agents : agents         
-    });
-});
+    res.send(agents);
+})
 
 module.exports = router;
